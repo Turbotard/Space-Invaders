cd8962da813bb19438b540f6ec785106a0a0431a
Aouadi <aouadilina04@gmail.com>
2023-01-04 23:07:12 +0100

    ALORS ON RECOMMENCE

--- canvas/Player.js
+++ canvas/Player.js
@@ -1,50 +1,61 @@
+// on définit la classe Player
 export default class Player {
-  rightPressed = false;
-  leftPressed = false;
-  shootPressed = false;
+  rightPressed = false;// on définit une variable pour savoir si la touche droite est enfoncée ou non
+  leftPressed = false;// la meme mais pr la gauche
+  shootPressed = false; // la meme mais pr le tir(espace)
 
-  constructor(canvas, velocity, bulletController) {
+
+  // on définit le constructeur de la classe Player
+  constructor(canvas, velocity, bulletController) {// on lui passe en paramètre le canvas, la vitesse et le controller de bullet
+    // on définit les propriétés de la classe Player
     this.canvas = canvas;
     this.velocity = velocity;
     this.bulletController = bulletController;
 
-    this.x = this.canvas.width / 2;
-    this.y = this.canvas.height - 75;
-    this.width = 50;
-    this.height = 48;
-    this.image = new Image();
-    this.image.src = "images/player.png";
-
-    document.addEventListener("keydown", this.keydown);
-    document.addEventListener("keyup", this.keyup);
+    // on définit les propriétés de l'image du joueur
+    this.x = this.canvas.width / 2;// on définit la position X du joueur
+    this.y = this.canvas.height - 75;// on définit la position Y du joueur
+   
+    this.height = 48;// on définit la hauteur du joueur
+    this.width = 70;// on définit la largeur du joueur
+    this.image = new Image(); // on définit une nouvelle img (mon joueur)
+    this.image.src = "images/player.jpg";
+    
+    document.addEventListener("keydown", this.keydown);// on définit une variable pour savoir si la touche droite est enfoncée ou non
+    document.addEventListener("keyup", this.keyup);// on définit une variable pour savoir si la touche gauche est enfoncée ou non
   }
 
+
+  // on définit une fonction pour DESSINER le JOUEUR
   draw(ctx) {
-    if (this.shootPressed) {
-      this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 10);
+    if (this.shootPressed) {// si la touche espace est enfoncée
+      // on tire une balle (position X du joueur + la moitié de sa largeur, la position Y du joueur, la vitesse et la taille de la balle
+      this.bulletController.shoot(this.x + this.width / 2, this.y, 5, 10);// on tire une balle
     }
-    this.move();
-    this.collideWithWalls();
+    
+    this.move();// on déplace le joueur
+    this.touchCanvasBorder();// on vérifie si le joueur touche LES COTÉS du canvas
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
-
-  collideWithWalls() {
-    //left
-    if (this.x < 0) {
-      this.x = 0;
+// on définit une fonction pour vérifier si le joueur touche les cotés du canvas
+  touchCanvasBorder() {
+    //gauche
+    if (this.x < 0) {// si la position X du joueur est inférieur à 0
+      this.x = 0;// on définit la position X du joueur à 0
     }
 
-    //right
-    if (this.x > this.canvas.width - this.width) {
-      this.x = this.canvas.width - this.width;
+    //droite
+    if (this.x > this.canvas.width - this.width) {// si la position X du joueur est supérieur à la largeur du canvas - la largeur du joueur
+      this.x = this.canvas.width - this.width;// on définit la position X du joueur à la largeur du canvas - la largeur du joueur
     }
   }
 
-  move() {
-    if (this.rightPressed) {
-      this.x += this.velocity;
-    } else if (this.leftPressed) {
-      this.x += -this.velocity;
+  move() {// on définit une fonction pour déplacer le joueur
+    if (this.rightPressed) {// si la touche droite est enfoncée
+      this.x += this.velocity;//  on ajoute la vitesse à la position X du joueur
+    } else if (this.leftPressed) {//si la touche gauche est enfoncée
+      this.x += -this.velocity;// on ajoute la vitesse à la position X du joueur
+
     }
   }
 
